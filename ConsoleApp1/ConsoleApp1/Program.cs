88a22d2854054e47122a123a8219897f113b8603
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-09-27 19:29:26 +0300

    HW3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус: ");
+            string r1 = Console.ReadLine();
+            int r2 = Convert.ToInt32(r1);
+            double radius = 3.14 * r2 * r2;
+            Console.Write("Площадь круга: "+ radius);
+            Console.ReadLine();
+        }
+    }
+}
