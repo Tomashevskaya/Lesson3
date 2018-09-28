0bd8e67d4d1186519409fe8d091fa955d7b3a22c
user <user@ECHTP-51-7>
2018-09-28 19:21:17 +0300

    qfv

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Hello world!");//342423423
             //hbvlfjvhd
+            Console.WriteLine("khjv kjdvh ");
         }
     }
 }
