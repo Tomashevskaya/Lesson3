c4bf19f22d7a29861f5b790eccdbe920efd0ee53
user <user@ECHTP-51-7>
2018-09-28 19:50:10 +0300

    Practice1 completed

---
+++ Lesson4-Practice1/Lesson4-Practice1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число Х");
+            String x1 = Console.ReadLine();
+            Console.WriteLine("Введите число Y");
+            String y1 = Console.ReadLine();
+            int x = Convert.ToInt32(x1);
+            int y = Convert.ToInt32(y1);
+            if (x >= 0 && y >= 0)
+            {
+                if (x == y) Console.WriteLine("Значения равны");
+                if (x > y) Console.WriteLine("Значение Х больше Y на " + (x - y));
+                if (x < y) Console.WriteLine("Значение Y больше Х на " + (y - x));
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
