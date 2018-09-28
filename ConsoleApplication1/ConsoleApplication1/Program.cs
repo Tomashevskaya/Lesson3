c9c8e5b5053c48b6096016953b577d27935fef2b
user <user@ECHTP-51-7>
2018-09-28 19:19:38 +0300

    cghjcghj

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");//342423423
-            Console.WriteLine("Hello");
             //hbvlfjvhd
         }
     }
