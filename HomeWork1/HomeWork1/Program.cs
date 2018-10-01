b275519073f75a13fb9d803cd9646b2ee87e7975
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-10-01 15:23:34 +0300

    HW4

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                if (i % 2 == 0) Console.WriteLine("i = " + i + " Четное!");
+                else Console.WriteLine("i = " + i + " Нечетное!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
