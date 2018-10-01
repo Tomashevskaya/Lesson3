c92ef9a3ee22ff5d42dbbbf7cb56df7cc806f227
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-10-01 16:26:35 +0300

    HW4

---
+++ HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово: ");
+            string str = Console.ReadLine();
+            int l = str.Length;
+            int k = 0;
+            for (int i = 0; i < l; i++)
+            {
+                if (str[i] == 'a') k += 1;
+            }
+            Console.WriteLine("Количество букв а = " + k);
+            Console.ReadLine();
+        }
+    }
+}
