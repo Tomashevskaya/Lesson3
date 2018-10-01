924af182d48afd76709d7159129267afc7cd4910
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-10-01 15:44:58 +0300

    HW4

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'Z'; i >= 'A'; i--)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
