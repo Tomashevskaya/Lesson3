a4689fb1ad44e2553871c5936a9519c34af0043a
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-10-01 17:05:58 +0300

    HW4

---
+++ HomeWork6/HomeWork6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово: ");
+            string str = Console.ReadLine();
+            int l = str.Length;
+            for (int i = l; i > 0; i--) Console.Write(str[i]); 
+            Console.ReadLine();
+        }
+    }
+}
