6a5da5d995464438aca3a16ebcdca0f102af48dd
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-10-12 15:50:08 +0300

    Lines_Poem

---
+++ Lines-Task2/Lines-Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lines_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввведите стихотворение: ");
+            string poem = Console.ReadLine();
+            string poem1 = poem.Replace("о", "а");
+            string poem2 = poem1.Replace("л", "ль");
+            string poem3 = poem2.Replace("ть", "т");
+            string[] poem4 = poem3.Split(',');
+            foreach (var item in poem4)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
