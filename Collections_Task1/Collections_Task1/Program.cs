78404a3fde52b9257e92cb0321c07a8bbe97853c
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-10-31 00:53:50 +0300

    Task1

---
+++ Collections_Task1/Collections_Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList poem = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Введите строку");
+                string s = Console.ReadLine();
+                poem.Add(s);
+            }
+            poem.Sort();
+            poem.RemoveAt(poem.Count - 1);
+            object[] poem2 = poem.ToArray();
+
+            foreach (var item in poem2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
