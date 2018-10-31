52caf8759e65ccc67fb27bb9caff0acc23d08b1c
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2018-10-31 11:33:03 +0300

    EX-1/2

--- L9_Practice/L9_Practice/Program.cs
+++ L9_Practice/L9_Practice/Program.cs
@@ -8,29 +8,6 @@ namespace L9_Practice
 {
     class Program
     {
-        /*static void Main(string[] args)
-        {
-           
-                Coordinates coord = new Coordinates() {x=1, y=2,z=3 };
-                (int x, int  y, int z) = coord;
-                double result = Math.Sqrt(x * x + y * y + z * z);
-                Console.WriteLine("Длина вектора равна {0}", result);
-        }
-
-
-        struct Coordinates
-        {
-            public int x { get; set; }
-            public int y { get; set; }
-            public int z { get; set; }
-        }
-        public void Deconstruct(out int X, out int Y, out int Z)
-        {
-            X = this.x;
-            Y = this.y;
-            Z = this.z;
-        }
-       }*/
         static void Main(string[] args)
         {
             Coordinates cords = new Coordinates { x = 1, y = 2, z = 3 };
